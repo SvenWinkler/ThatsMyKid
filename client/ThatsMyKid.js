c982e644d0e26a1908e2e30a9150617a921a52f6
fabianschiller <fabian.schiller@gmail.com>
2013-07-23 18:22:55 +0200

    google+

--- client/ThatsMyKid.js
+++ client/ThatsMyKid.js
@@ -1,20 +1,47 @@
 if (Meteor.is_client) {
     Meteor.startup(function () {
         $(document).ready(function (){
-            $('#transparent_layer').bind('click', function() {
-                $('#transparent_layer').css("visibility", "hidden")
-                $('#photo_box').css("visibility", "hidden")
-            })
-            $('#photo_box').bind('click', function() {
-                $('#transparent_layer').css("visibility", "hidden")
-                $('#photo_box').css("visibility", "hidden")
-            })
-            $('#walking').bind('click', function() {
-                $('#transparent_layer').css("visibility", "visible")
-                $('#photo_box').css("visibility", "visible")
-            });
+            Visibility.bindClickHandlers()
         });
     });
+}
+
+Visibility = {}
+
+Visibility.bindClickHandlers = function() {
+    $('#transparent_layer').on('click', function() {
+        Visibility.hidePhoto()
+    })
+    $('#photo_box').on('click', function() {
+        Visibility.hidePhoto()
+    })
+//    $('#status').bind('click', function () {
+//        Visibility.showPhoto()
+//    })
+   $('#gallery').find('img').map(function(){
+       alert(this.src)
+       this.on('click', function() {
+           alert("Test2")
+           Visibility.showPhoto(this.src);
+       })
+    });
+}
+
+Visibility.showPhoto = function(url) {
+    alert("showPhoto")
+    $('photo_box').find('img').src = url
+    Visibility.showPhoto()
+}
+
+Visibility.showPhoto = function() {
+    alert("showPhoto2")
+    $('#transparent_layer').css("visibility", "visible")
+    $('#photo_box').css("visibility", "visible")
+}
+
+Visibility.hidePhoto = function() {
+    $('#transparent_layer').css("visibility", "hidden")
+    $('#photo_box').css("visibility", "hidden")
 }
 
 Template.banner.greeting = function () {
