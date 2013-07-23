574ad6c0609b0f74019c51e1dff212193bb370da
svnwnk <svenwink@gmx.de>
2013-07-23 18:08:28 +0200

    added static image list

---
+++ client/views/posts/posts_list.js
@@ -0,0 +1,66 @@
+var postsData = [ 
+{
+	url: 'swimming.jpg'
+},
+{
+	url: 'thinking.jpg'
+},
+{
+	url: 'hugging.jpg'
+},
+{
+	url: 'art.jpg'
+},
+{
+	url: 'driving.jpg'
+},
+{
+	url: 'babysleeping.jpg'
+},
+{
+	url: 'looking.jpg'
+},
+{
+	url: 'painting.jpg'
+},
+{
+	url: 'climbing.jpg'
+},
+{
+	url: 'playing.jpg'
+},
+{
+	url: 'swimming.jpg'
+},
+{
+	url: 'thinking.jpg'
+},
+{
+	url: 'hugging.jpg'
+},
+{
+	url: 'art.jpg'
+},
+{
+	url: 'driving.jpg'
+},
+{
+	url: 'babysleeping.jpg'
+},
+{
+	url: 'looking.jpg'
+},
+{
+	url: 'painting.jpg'
+},
+{
+	url: 'climbing.jpg'
+},
+{
+	url: 'playing.jpg'
+}
+];
+
+
+Template.postsList.helpers({ posts: postsData
+});
