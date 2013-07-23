3110440d82b19a7835be922fae7ca5adb1fd7c7a
svnwnk <svenwink@gmx.de>
2013-07-23 18:42:08 +0200

    changed pictures

--- client/views/posts/events_list.js
+++ client/views/posts/events_list.js
@@ -1,6 +1,7 @@
 var eventsData = [ 
 {
-	url: 'swimming.jpg'
+	url: 'exploring.jpg',
+	comment: 'Walter De Maria, Vertical Earth Kilometer in Kassel, Germany. Own picture, taken 1997. Child, my daughter, is two years old at time photo was taken.'
 },
 {
 	url: 'thinking.jpg'
@@ -12,7 +13,8 @@ var eventsData = [
 	url: 'art.jpg'
 },
 {
-	url: 'driving.jpg'
+	url: 'laughing.jpg',
+	comment: 'By http://www.flickr.com/photos/vatobob/ [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons'
 },
 {
 	url: 'babysleeping.jpg'
@@ -21,16 +23,19 @@ var eventsData = [
 	url: 'looking.jpg'
 },
 {
-	url: 'painting.jpg'
+	url: 'admiring.png',
+	comment: 'By Victor.mv (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons'
 },
 {
-	url: 'climbing.jpg'
+	url: 'climbing.jpg',
+	comment: 'By Loadmaster (David R. Tribble)    This image was made by Loadmaster (David R. Tribble) Email the author:  David R. Tribble Also see my personal gallery at Google Picasa (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0) or GFDL (http://www.gnu.org/copyleft/fdl.html)], via Wikimedia Commons'
 },
 {
 	url: 'playing.jpg'
 },
 {
-	url: 'swimming.jpg'
+	url: 'exploring.jpg',
+	comment: 'Walter De Maria, Vertical Earth Kilometer in Kassel, Germany. Own picture, taken 1997. Child, my daughter, is two years old at time photo was taken.'
 },
 {
 	url: 'thinking.jpg'
@@ -42,7 +47,8 @@ var eventsData = [
 	url: 'art.jpg'
 },
 {
-	url: 'driving.jpg'
+	url: 'laughing.jpg',
+	comment: 'By http://www.flickr.com/photos/vatobob/ [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons'
 },
 {
 	url: 'babysleeping.jpg'
@@ -51,10 +57,12 @@ var eventsData = [
 	url: 'looking.jpg'
 },
 {
-	url: 'painting.jpg'
+	url: 'painting.jpg',
+	comment: 'By Jim Pennucci from Hope, USA (Painting) [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons'
 },
 {
-	url: 'climbing.jpg'
+	url: 'climbing.jpg',
+	comment: 'By Loadmaster (David R. Tribble)    This image was made by Loadmaster (David R. Tribble) Email the author:  David R. Tribble Also see my personal gallery at Google Picasa (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0) or GFDL (http://www.gnu.org/copyleft/fdl.html)], via Wikimedia Commons'
 },
 {
 	url: 'playing.jpg'
