4418570bb9c4219acaa960fdeca06a5cb5514fdd
fabianschiller <fabian.schiller@gmail.com>
2013-07-24 10:47:29 +0200

    Merge branch 'master' of https://github.com/SvenWinkler/ThatsMyKid
    Conflicts:
    	client/ThatsMyKid.html

--- client/ThatsMyKid.js
+++ client/ThatsMyKid.js
@@ -9,49 +9,33 @@ if (Meteor.is_client) {
 Visibility = {}
 
 Visibility.bindClickHandlers = function() {
-    $('#transparent_layer').on('click', function() {
+    "use strict";
+    $('#photo_viewer').on('click', function() {
         Visibility.hidePhoto()
     })
-    $('#photo_box').on('click', function() {
-        Visibility.hidePhoto()
-    })
-//    $('#status').bind('click', function () {
-//        Visibility.showPhoto()
-//    })
-   $('#gallery').find('img').map(function(){
-       alert(this.src)
-       this.on('click', function() {
-           alert("Test2")
-           Visibility.showPhoto(this.src);
-       })
-    });
 }
 
 Visibility.showPhoto = function(url) {
-    alert("showPhoto")
-    $('photo_box').find('img').src = url
-    Visibility.showPhoto()
-}
-
-Visibility.showPhoto = function() {
-    alert("showPhoto2")
-    $('#transparent_layer').css("visibility", "visible")
-    $('#photo_box').css("visibility", "visible")
+    "use strict";
+    $('#photo_box_img').attr("src", url);
+    $('#photo_viewer').css("visibility", "visible");
 }
 
 Visibility.hidePhoto = function() {
-    $('#transparent_layer').css("visibility", "hidden")
-    $('#photo_box').css("visibility", "hidden")
+    "use strict";
+    $('#photo_viewer').css("visibility", "hidden")
 }
 
 Template.banner.greeting = function () {
     return "ThatsMyKid";
-};
+}
 
+/*
 Template.hello.events({
   'click input' : function () {
+      "use strict";
     // template data, if any, is available in 'this'
     if (typeof console !== 'undefined')
       console.log("You pressed the button");
   }
-});
+})*/
--- client/views/posts/event_item.js
+++ client/views/posts/event_item.js
@@ -5,3 +5,10 @@
  * Time: 09:03
  * To change this template use File | Settings | File Templates.
  */
+Template.eventItem.events(
+    {
+        'click': function(e) {
+            Visibility.showPhoto(e.target.src)
+        }
+    }
+);
