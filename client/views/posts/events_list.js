e7f73e88219c24ecb37fbaf7d310f2d094ac887c
svnwnk <svenwink@gmx.de>
2013-07-24 10:01:51 +0200

    added event attribute to event

--- client/views/posts/events_list.js
+++ client/views/posts/events_list.js
@@ -1,71 +1,103 @@
 var eventsData = [ 
 {
 	url: 'exploring.jpg',
-	comment: 'Walter De Maria, Vertical Earth Kilometer in Kassel, Germany. Own picture, taken 1997. Child, my daughter, is two years old at time photo was taken.'
+	comment: 'Walter De Maria, Vertical Earth Kilometer in Kassel, Germany. Own picture, taken 1997. Child, my daughter, is two years old at time photo was taken.',
+	event: '1. Urlaub'
 },
 {
-	url: 'thinking.jpg'
+	url: 'thinking.jpg',
+	comment: '',
+	event: 'Geburtstag'
 },
 {
-	url: 'hugging.jpg'
+	url: 'hugging.jpg',
+	comment: '',
+	event: '1. Zähne'
 },
 {
-	url: 'art.jpg'
+	url: 'art.jpg',
+	comment: '',
+	event: 'Geburtstag'
 },
 {
 	url: 'laughing.jpg',
-	comment: 'By http://www.flickr.com/photos/vatobob/ [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons'
+	comment: 'By http://www.flickr.com/photos/vatobob/ [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons',
+	event: '1. Photoshooting'
 },
 {
-	url: 'babysleeping.jpg'
+	url: 'babysleeping.jpg',
+	comment: '',
+	event: 'Geburt'
 },
 {
-	url: 'looking.jpg'
+	url: 'looking.jpg',
+	comment: '',
+	event: 'Kindergarten'
 },
 {
 	url: 'admiring.png',
-	comment: 'By Victor.mv (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons'
+	comment: 'By Victor.mv (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0)], via Wikimedia Commons',
+	event: '1. Ausflug'
 },
 {
 	url: 'climbing.jpg',
-	comment: 'By Loadmaster (David R. Tribble)    This image was made by Loadmaster (David R. Tribble) Email the author:  David R. Tribble Also see my personal gallery at Google Picasa (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0) or GFDL (http://www.gnu.org/copyleft/fdl.html)], via Wikimedia Commons'
+	comment: 'By Loadmaster (David R. Tribble)    This image was made by Loadmaster (David R. Tribble) Email the author:  David R. Tribble Also see my personal gallery at Google Picasa (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0) or GFDL (http://www.gnu.org/copyleft/fdl.html)], via Wikimedia Commons',
+	event: 'Kletternausflug'
 },
 {
-	url: 'playing.jpg'
+	url: 'playing.jpg',
+	comment: '',
+	event: 'Geburtstag'
 },
 {
 	url: 'exploring.jpg',
-	comment: 'Walter De Maria, Vertical Earth Kilometer in Kassel, Germany. Own picture, taken 1997. Child, my daughter, is two years old at time photo was taken.'
+	comment: 'Walter De Maria, Vertical Earth Kilometer in Kassel, Germany. Own picture, taken 1997. Child, my daughter, is two years old at time photo was taken.',
+	event: '1. Urlaub'
 },
 {
-	url: 'thinking.jpg'
+	url: 'thinking.jpg',
+	comment: '',
+	event: 'Geburtstag'
 },
 {
-	url: 'hugging.jpg'
+	url: 'hugging.jpg',
+	comment: '',
+	event: '1. Zähne'
 },
 {
-	url: 'art.jpg'
+	url: 'art.jpg',
+	comment: '',
+	event: 'Geburtstag'
 },
 {
 	url: 'laughing.jpg',
-	comment: 'By http://www.flickr.com/photos/vatobob/ [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons'
+	comment: 'By http://www.flickr.com/photos/vatobob/ [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons',
+	event: '1. Photoshooting'
 },
 {
-	url: 'babysleeping.jpg'
+	url: 'babysleeping.jpg',
+	comment: '',
+	event: 'Geburt'
 },
 {
-	url: 'looking.jpg'
+	url: 'looking.jpg',
+	comment: '',
+	event: 'Kindergarten'
 },
 {
 	url: 'painting.jpg',
-	comment: 'By Jim Pennucci from Hope, USA (Painting) [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons'
+	comment: 'By Jim Pennucci from Hope, USA (Painting) [CC-BY-2.0 (http://creativecommons.org/licenses/by/2.0)], via Wikimedia Commons',
+	event: 'Kindergarten'
 },
 {
 	url: 'climbing.jpg',
-	comment: 'By Loadmaster (David R. Tribble)    This image was made by Loadmaster (David R. Tribble) Email the author:  David R. Tribble Also see my personal gallery at Google Picasa (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0) or GFDL (http://www.gnu.org/copyleft/fdl.html)], via Wikimedia Commons'
+	comment: 'By Loadmaster (David R. Tribble)    This image was made by Loadmaster (David R. Tribble) Email the author:  David R. Tribble Also see my personal gallery at Google Picasa (Own work) [CC-BY-SA-3.0 (http://creativecommons.org/licenses/by-sa/3.0) or GFDL (http://www.gnu.org/copyleft/fdl.html)], via Wikimedia Commons',
+	event: 'Kletternausflug'
 },
 {
-	url: 'playing.jpg'
+	url: 'playing.jpg',
+	comment: '',
+	event: 'Geburtstag'
 }
 ];
 
